a9d20c378091c0ddc74f79aa9fcad48ef5585048
Jack <ranchayasambath@gmail.com>
2022-12-13 16:05:04 -0600

    update changes

--- gui-auto.py
+++ gui-auto.py
@@ -1,15 +1,29 @@
-import tkinter, customtkinter
+import tkinter, customtkinter,re ,np
 
 customtkinter.set_appearance_mode("System") # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue") # Themes: blue (default), dark-blue, green
 
 app = customtkinter.CTk() # create CTk window like you do with the Tk window
+
 app.geometry("400x240")
 app.title("Calculator")
 
-
 def button_function():
-    print("button pressed")
+    # print("button pressed")
+    usrInput = input("Stop asking me to calculate, Enter dir path: ")
+# text = open('C:/Users/ranch/Downloads/jack.txt')
+    text = open(usrInput)
+    final = []
+    for line in text:
+        line = line.strip()
+        match = re.findall('(?<=logical reads )[^,]*',line)
+
+        if len(match) > 0:
+            lineVal = (sum(map(int, match))*8096)*1000000
+            final.append(lineVal)
+            #  print("line sum = {0}".format(lineVal))
+    write = open('result.txt' , 'w')
+    write.write("Final sum = {: ,} ".format(np.sum(final)) + "MB") 
 
 # Use CTkButton instead of tkinter Button
 button = customtkinter.CTkButton(master=app, text="Calculate", command=button_function)
---
+++ gui-auto2.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import ttk
+import re,np
+window =tk.Tk()
+
+window.title("Python Calculator")
+window.minsize(600,400)
+
+def clickMe():
+    text = open(input.get())
+    final = []
+    for line in text:
+        line = line.strip()
+        match = re.findall('(?<=logical reads )[^,]*',line)
+
+        if len(match) > 0:
+            lineVal = (sum(map(int, match))*8096)/1000000
+            final.append(lineVal)
+    label.configure(text= ("Final sum = {: ,} ".format(int(np.sum(final))) + "MB") )
+
+label = ttk.Label(window, text = "Enter your path: ")
+label.grid(column = 0, row=0)
+
+input = tk.StringVar()
+pathInput = ttk.Entry(window, width=100, textvariable= input)
+pathInput.grid(column=0, row=1)
+
+button = ttk.Button(window, text = "Calculate", command = clickMe)
+button.grid(column=0)
+
+window.mainloop()
