bb4f2cdcf7aa8009738d8aacc3fb72892eefb740
Jack <ranchayasambath@gmail.com>
2022-12-19 14:39:11 -0600

    update changes

--- gui-auto4.py
+++ gui-auto4.py
@@ -11,16 +11,19 @@ def clickMe():
     userInput = window.clipboard_get()
     userInput = userInput.strip()
     match = re.findall('(?<=logical reads )[^,]*',userInput)
-    for i in match:
-        final.append(int(i))
-    # print(final)
-    total = 0
-    for ele in range(0, len(final)):
-        total = total + final[ele]
-    sum = (total*8096)/1000000
-    finalSum = int(round(sum))
-    # print(finalSum)
-    label.configure(text= "Your Total Sum is :  " +  str(finalSum) +  "  MB")
+    if len(match) > 0:
+        for i in match:
+            final.append(int(i))
+        # print(final)
+        total = 0
+        for ele in range(0, len(final)):
+            total = total + final[ele]
+        sum = (total*8096)/1000000
+        finalSum = int(round(sum))
+        # print(finalSum)
+        label.configure(text= "Your Total Sum is :  " +  str(finalSum) +  "  MB")
+    else:
+        label.configure(text= "You did not copy the right data!! Stupid")
 label = ttk.Label(window, text = "Ctrl + C your data: ")
 label.grid(column = 0, row=0)
 
